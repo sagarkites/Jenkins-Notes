b66d7acf7074285c700846ae5b9aac47f6d6dce0
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2019-08-23 18:36:17 +0530

    Update Jenkins.sh
    shell script

--- Jenkins.sh
+++ Jenkins.sh
@@ -6,5 +6,5 @@ curl -X GET http://user:api_key@jenkins_url:8080/job/project_name/config.xml -H 
 array=(Job_1 Job_2 Job_3 Job_4)
 for i in "${array[@]}"
 do
-   curl -s -X POST 'http://user:116b6c137963cb4cd67ad53588470b3f0b@jenkins_url:8080/createItem?name='$i --data-binary @jobconfig.xml -H "$CRUMB" -H "Content-Type:text/xml"
+   curl -s -X POST 'http://user:api_key@jenkins_url:8080/createItem?name='$i --data-binary @jobconfig.xml -H "$CRUMB" -H "Content-Type:text/xml"
 done
